c2cb4c3f78ba90218ad85e4ee13d462b66b6550d
Delight Sheriff <delightsheriff@gmail.com>
2024-06-30 10:07:24 +0100

    adding fonts

---
+++ app/ui/fonts.ts
@@ -0,0 +1,8 @@
+import {Inter} from 'next/font/google'
+import {Lusitana} from 'next/font/google'
+
+
+// Adding fonts to the application
+
+export const inter = Inter({subsets: ['latin']})
+export const lusitana = Lusitana({subsets: ['latin'], weight: ['400', '700']})
