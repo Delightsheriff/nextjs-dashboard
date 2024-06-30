3c56d1e1b01954b634e34f05573b6ef78cb13274
Delight Sheriff <delightsheriff@gmail.com>
2024-06-30 23:58:40 +0100

    adding metadata

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,12 @@
-export default function Page() {
-    return <p>Customers Page</p>;
-  }
+// import { fetchCustomers } from '@/app/lib/data';
+// import CustomersTable from '@/app/ui/customers/table';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Customers',
+};
+
+// export default async function Page() {
+//   const customers = await fetchCustomers();
+//     return <CustomersTable customers={customers} />;
+//   }
